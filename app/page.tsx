630c79b836cc25cf167c3b4f7c9934b40301ec60
Ausaf Ul Islam <165744535+Ausaf-ul-Islam@users.noreply.github.com>
2024-11-06 23:15:38 +0500

    ⚜Next.js Assignment 05⚜

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import HeroSection from "./components/Hero";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <HeroSection />
+    </div>
+  );
+}
